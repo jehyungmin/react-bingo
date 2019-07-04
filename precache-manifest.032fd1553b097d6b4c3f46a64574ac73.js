ebdfe734c51e37341d0f1efaada5faa5de9da1e9
jehyungmin <wpaudrl@gmail.com>
2019-07-04 13:31:15 +0900

    Updates

---
+++ precache-manifest.032fd1553b097d6b4c3f46a64574ac73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5dbe41b63037043cf443e936a98efc",
+    "url": "/react-bingo/index.html"
+  },
+  {
+    "revision": "f37fa17f27c2042ddddd",
+    "url": "/react-bingo/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "9ccb6f585b2aea8d9e66",
+    "url": "/react-bingo/static/css/main.9043c523.chunk.css"
+  },
+  {
+    "revision": "f37fa17f27c2042ddddd",
+    "url": "/react-bingo/static/js/2.d303edc9.chunk.js"
+  },
+  {
+    "revision": "9ccb6f585b2aea8d9e66",
+    "url": "/react-bingo/static/js/main.8c81d046.chunk.js"
+  },
+  {
+    "revision": "40d97466779c5abfc5e6",
+    "url": "/react-bingo/static/js/runtime~main.67983e0f.js"
+  }
+]);
